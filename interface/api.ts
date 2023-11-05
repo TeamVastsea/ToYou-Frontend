9e1a4b746b5e6d4b21e7b8d4d0c4896eb50bdb9d
zrll_ <zhengruilin0@126.com>
2023-11-05 23:29:35 +0800

    add: register

--- interface/api.ts
+++ interface/api.ts
@@ -1,1 +1,32 @@
-export const SERVER_URL = "https://127.0.0.1:5678";
+export const SERVER_URL = "http://127.0.0.1:8102";
+
+export async function checkEmail(email: string): Promise<boolean> {
+    //request
+    var requestOptions: RequestInit = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    let response = await fetch(SERVER_URL + "/user/email/" + email, requestOptions);
+
+    return await response.text() == "exists";
+}
+
+export async function creatUser(email: string, password: string, username: string, code: string): Promise<boolean> {
+
+    var formdata = new FormData();
+    formdata.append("email", email);
+    formdata.append("password", password);
+    formdata.append("username", username);
+    formdata.append("code", code);
+
+    var requestOptions: RequestInit = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    let response = await fetch(SERVER_URL + "/user", requestOptions);
+
+    return response.ok;
+}
