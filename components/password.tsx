dc1bc5a580fe9f3330267773d6412ca9b43fedde
GaoNeng-wWw <gaonenggg@outlook.com>
2023-11-23 15:27:49 +0800

    fix(authenticate): better register

---
+++ components/password.tsx
@@ -0,0 +1,39 @@
+import { Input } from "@nextui-org/react";
+import { EyeFilledIcon, EyeSlashFilledIcon } from "@nextui-org/shared-icons";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+export interface PasswordProps {
+    label: string;
+    placeholder: string;
+    key: string
+    value?: string;
+    onValueChange?: (val:string)=>void;
+}
+
+export default function Password(
+    props: PasswordProps
+){
+    const {key, label, placeholder, value, onValueChange} = props
+    const [visible, setVisible] = useState(false);
+    const toggleVisible = () => setVisible(!visible);
+    const endContent = () => {
+        return (<button onClick={toggleVisible}>
+            {visible ? (
+                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+            ) : (
+                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+            )}
+        </button>)
+    }
+    return (
+        <Input
+            key={key}
+            type={visible ? 'text' : 'password'}
+            label={label}
+            placeholder={placeholder}
+            value={value}
+            onValueChange={onValueChange}
+            endContent={endContent()}
+        />
+    )
+}
