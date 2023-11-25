76c6a7f27427f83712ef365409a7a0548693e86a
GaoNeng <31283122+GaoNeng-wWw@users.noreply.github.com>
2023-11-25 16:04:02 +0800

    fix: page-type (#30)

--- app/authenticate/page.tsx
+++ app/authenticate/page.tsx
@@ -208,7 +208,7 @@ const Register = (
 export default function Page() {
     const [email, setEmail] = useState('');
     const [userInput, setUserInput] = useState('');
-    const [pageType, setPageType] = useState<PageType>('register');
+    const [pageType, setPageType] = useState<PageType>('wait-check');
     const {buttonMessage} = useButtonMessage(pageType, '下一步');
     const [policyState, setPolicyState] = useState(false);
     const [loading, setLoading] = useState(false);
