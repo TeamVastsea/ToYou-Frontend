63e8886ce6aee715c5f7175b7fbac27f78bf693b
GaoNeng-wWw <gaonenggg@outlook.com>
2023-12-06 21:10:51 +0800

    fix: fix type error

--- app/store.ts
+++ app/store.ts
@@ -1,4 +1,4 @@
-import { UserModel } from '@/interface/model/user';
+import { UserModel } from '../interface/model/user';
 import {atom} from 'jotai';
 
 export const profile = atom<null | UserModel>(null);
