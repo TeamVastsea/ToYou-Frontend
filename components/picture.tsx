e8dfca29e6326ccd38d18f16af52016add6177b9
zrll_ <zhengruilin0@126.com>
2023-12-12 21:21:48 +0800

    fix get share link when loading

--- components/picture.tsx
+++ components/picture.tsx
@@ -1,6 +1,5 @@
-import React, {useEffect, useMemo, useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {
-    Selection,
     Button,
     Card,
     CardFooter,
@@ -10,10 +9,8 @@ import {
     Popover,
     PopoverContent,
     PopoverTrigger,
-    useDisclosure,
-    Dropdown,
-    DropdownTrigger,
-    PopoverProvider
+    Selection,
+    useDisclosure
 } from "@nextui-org/react";
 import {getImgContrast} from 'react-img-contrast';
 import copy from "copy-to-clipboard";
@@ -27,33 +24,33 @@ import {SERVER_URL} from "@/interface/api";
 import {PriceInfo} from "@/components/price";
 import IOC from "@/providers";
 
-const SharedButton = (props: {link: string, pid?:string, className?: string}) => {
+const SharedButton = (props: { link: string, pid?: string, className?: string }) => {
     const {link, pid} = props;
     const [isChecked, setIsChecked] = useState(false);
     let timeout: NodeJS.Timeout | null = null;
     return (
         <Button isIconOnly className="justify-center"
-            variant="bordered" size="sm"
-            onClick={() => {
-                if (pid){
-                    PictureAPI.sharePicture(pid).then((r) => {
-                        copy(SERVER_URL + "/picture/share/" + r.sid);
-                        Message.success("已经复制到剪贴板");
-                    });
-                }
-                if (link){
-                    copy(link);
-                }
-                setIsChecked(true);
-                if (timeout != null) {
-                    clearTimeout(timeout);
-                    timeout = null;
-                }
-                timeout = setTimeout(() => {
-                    setIsChecked(false);
-                    timeout = null;
-                }, 1000);
-            }}>
+                variant="bordered" size="sm"
+                onClick={() => {
+                    if (!link && pid) {
+                        PictureAPI.sharePicture(pid).then((r) => {
+                            copy(SERVER_URL + "/picture/share/" + r.sid);
+                            Message.success("已经复制到剪贴板");
+                        });
+                    }
+                    if (link) {
+                        copy(link);
+                    }
+                    setIsChecked(true);
+                    if (timeout != null) {
+                        clearTimeout(timeout);
+                        timeout = null;
+                    }
+                    timeout = setTimeout(() => {
+                        setIsChecked(false);
+                        timeout = null;
+                    }, 1000);
+                }}>
             {isChecked ? <CheckLinearIcon className={props.className}/> : <FiCopy className={props.className}/>}
         </Button>
     )
@@ -66,7 +63,7 @@ export default function Picture(props: PictureProps) {
     let [saveLoading, setSaveLoading] = useState(false);
     const [delLoading, setDelLoading] = useState(false);
     const [selectedKeys, setSelectedKeys] = useState<Selection>(new Set(['none']));
-    
+
     const [textColor, setTextColor] = useState('text-black');
     let originName = props.name;
 
@@ -80,35 +77,29 @@ export default function Picture(props: PictureProps) {
     const deletePicture = (onClose: () => void) => {
         setDelLoading(true);
         IOC.picture.deletePicture(props.pid)
-        .then(()=>{
-            Message.success('删除成功')
-            props.onDelete?.(props.pid);
-        })
-        .catch((reason) => {
-            Message.error(reason)
-        })
-        .finally(() => {
-            onClose();
-            setDelLoading(false)
-        })
+            .then(() => {
+                Message.success('删除成功')
+                props.onDelete?.(props.pid);
+            })
+            .catch((reason) => {
+                Message.error(reason)
+            })
+            .finally(() => {
+                onClose();
+                setDelLoading(false)
+            })
     }
-
-    useMemo(()=>{
-        const key = Array.from(selectedKeys).join('');
-        let mode = key == "watermark" ? 1 : key == "compressed" ? 2 : 3;
-        generateShareLink(mode);
-    }, [selectedKeys])
 
     const footerRef = useRef(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         getImgContrast({
             imgSrc: props.url,
         })
-        .then((textState) => {
-            setTextColor(textState === 'white' ? 'text-white' : 'text-black')
-        })
-    },[props.url, setTextColor])
+            .then((textState) => {
+                setTextColor(textState === 'white' ? 'text-white' : 'text-black')
+            })
+    }, [props.url, setTextColor])
 
 
     return (
@@ -131,23 +122,24 @@ export default function Picture(props: PictureProps) {
                 />
                 <CardFooter
                     className={
-                    `
-                    w-auto max-w-[75%] justify-between
-                    before:bg-white/10 before:rounded-xl
-                    border-white/20 overflow-hidden py-2 absolute rounded-large bottom-1 shadow-small ml-1 z-10 space-x-2
-                    ${textColor}
-                    `
+                        `
+                        w-auto max-w-[75%] justify-between
+                        before:bg-white/10 before:rounded-xl
+                        border-white/20 overflow-hidden py-2 absolute rounded-large bottom-1 shadow-small ml-1 z-10 space-x-2
+                        ${textColor}
+                        `
                     }
                 >
                     <div ref={footerRef} className='w-fit flex gap-1 justify-center items-center'>
                         <p className={`text-tiny font-mono truncate ${textColor}`} title={name}>{name}</p>&nbsp;
                         <SharedButton link={link} pid={props.pid} className={
                             `${textColor}`
-                        } />
+                        }/>
                         <Popover>
                             <PopoverTrigger>
-                                <Button isIconOnly className="justify-center" variant="bordered" size="sm" isLoading={delLoading}>
-                                    {!delLoading && <DeleteIcon className={`${textColor}`} />}
+                                <Button isIconOnly className="justify-center" variant="bordered" size="sm"
+                                        isLoading={delLoading}>
+                                    {!delLoading && <DeleteIcon className={`${textColor}`}/>}
                                 </Button>
                             </PopoverTrigger>
                             <PopoverContent>
@@ -155,7 +147,9 @@ export default function Picture(props: PictureProps) {
                                     <div className="px-2 py-1">
                                         <h3 className="text-lg" {...titleStyle}>确定要删除吗</h3>
                                         <div className="my-2">
-                                            <Button color="danger" size="sm" fullWidth onClick={()=>deletePicture(()=>{})}>确认</Button>
+                                            <Button color="danger" size="sm" fullWidth
+                                                    onClick={() => deletePicture(() => {
+                                                    })}>确认</Button>
                                         </div>
                                     </div>
                                 )}
@@ -193,20 +187,25 @@ export default function Picture(props: PictureProps) {
                                                 disallowEmptySelection
                                                 selectionMode="single"
                                                 selectedKeys={selectedKeys}
-                                                onSelectionChange={setSelectedKeys}
+                                                onSelectionChange={(e) => {
+                                                    setSelectedKeys(e);
+                                                    const key = Array.from(e).join('');
+                                                    let mode = key == "watermark" ? 1 : key == "compressed" ? 2 : 3;
+                                                    generateShareLink(mode);
+                                                }}
                                                 disabledKeys={props.group == undefined ? [] : props.group.disabled}
                                             >
                                                 <ListboxItem key="none">不公开</ListboxItem>
                                                 <ListboxItem key="watermark">公开水印版本</ListboxItem>
                                                 <ListboxItem key="compressed"
-                                                            description={props.group?.name! == "professional" || props.group?.name! == "advanced" || props.group?.name! == "started" ? "" : "仅入门或以上套餐"}>公开压缩版本</ListboxItem>
+                                                             description={props.group?.name! == "professional" || props.group?.name! == "advanced" || props.group?.name! == "started" ? "" : "仅入门或以上套餐"}>公开压缩版本</ListboxItem>
                                                 <ListboxItem key="original"
-                                                            description={props.group?.name! == "professional" || props.group?.name! == "advanced" ? "" : "仅进阶或专业套餐"}>公开原图</ListboxItem>
+                                                             description={props.group?.name! == "professional" || props.group?.name! == "advanced" ? "" : "仅进阶或专业套餐"}>公开原图</ListboxItem>
                                             </Listbox>
                                         </PopoverContent>
                                     </Popover> : <div className="flex" style={{width: 450}}>
                                         <Input className={"font-mono"} variant={"underlined"} value={link} endContent={
-                                            <SharedButton link={link} />
+                                            <SharedButton link={link}/>
                                         }></Input>
 
                                     </div>}
@@ -245,6 +244,6 @@ export type PictureProps = {
     name: string,
     pubicMode?: string,
     group?: PriceInfo,
-    onPress?: ()=>void;
-    onDelete?: (pid:string)=>void;
+    onPress?: () => void,
+    onDelete?: (pid: string) => void,
 }
