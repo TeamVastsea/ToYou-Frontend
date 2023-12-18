f461482bbeffa26299a4cfc60f864e1eff85901b
zrll_ <zhengruilin0@126.com>
2023-12-18 23:19:55 +0800

    add table content

--- components/share-table.tsx
+++ components/share-table.tsx
@@ -1,37 +1,54 @@
 import React from "react";
-import { Table, TableBody, TableColumn, TableHeader, TableRow, TableCell } from "@nextui-org/table";
+import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/table";
 import {Spinner} from "@nextui-org/react";
-import { useSharedLinks } from "@/app/setting/hooks/use-shared-links";
+import {useSharedLinks} from "@/app/setting/hooks/use-shared-links";
 import {Link} from "@nextui-org/link";
-import {Button} from "@nextui-org/button";
+import {Button, ButtonGroup} from "@nextui-org/button";
+import {FaLock, FaLockOpen} from "react-icons/fa6";
+import {FiClipboard, FiEdit3, FiTrash2} from "react-icons/fi";
+import {Pagination} from "@nextui-org/pagination";
 
-export default function ShareTable({loading, list}: ReturnType<typeof useSharedLinks>) {
-    return(
+export default function ShareTable({loading, list, page, current, setCurrent}: ReturnType<typeof useSharedLinks>) {
+    return (
         <p className="text-left">
             <Table aria-label="Example static collection table"
-            classNames={{
-                wrapper: 'max-h-[400px]'
-            }}>
+                   classNames={{
+                       wrapper: 'max-h-[400px]'
+                   }} bottomContent={
+                page > 1 ?
+                    <div className="flex w-full justify-center">
+                        <Pagination
+                            isCompact
+                            showControls
+                            showShadow
+                            color="secondary"
+                            page={current}
+                            total={page}
+                            onChange={(page) => setCurrent(page)}
+                        />
+                    </div> : <></>
+            }>
                 <TableHeader>
-                    <TableColumn>链接</TableColumn>
+                    <TableColumn>文件名</TableColumn>
                     <TableColumn>密码</TableColumn>
                     <TableColumn>有效期</TableColumn>
                     <TableColumn>下载次数</TableColumn>
                     <TableColumn>操作</TableColumn>
                 </TableHeader>
-                <TableBody 
-                    loadingContent={<Spinner label="loading" />}
-                    loadingState={loading}
-                    emptyContent={"你没有上传数据"}
-                    items={list == undefined ? [] : list}
-                >
+                <TableBody loadingContent={<Spinner/>} emptyContent={"这里没有数据哦"} loadingState={loading} items={list}>
                     {(item) => (
                         <TableRow>
-                            <TableCell><Link>{item.sid}</Link></TableCell>
-                            <TableCell>{item.password ? "已设置" : "无"}</TableCell>
-                            <TableCell>{item.expiry}</TableCell>
+                            <TableCell><Link>{item.fileName}</Link></TableCell>
+                            <TableCell>{item.password ? <FaLock/> : <FaLockOpen/>}</TableCell>
+                            <TableCell>{new Date(item.expiry).toLocaleString()}</TableCell>
                             <TableCell>{item.downloads}</TableCell>
-                            <TableCell><Button>hello</Button></TableCell>
+                            <TableCell>
+                                <ButtonGroup>
+                                    <Button size={"sm"} isIconOnly><FiEdit3/></Button>
+                                    <Button size={"sm"} isIconOnly><FiClipboard/></Button>
+                                    <Button size={"sm"} isIconOnly color={"danger"}><FiTrash2/></Button>
+                                </ButtonGroup>
+                            </TableCell>
                         </TableRow>
                     )}
                 </TableBody>
