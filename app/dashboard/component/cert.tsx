6360f15ed5c69742d7eace36b8d09a9c764e1ce9
zrll_ <zhengruilin0@126.com>
2024-01-18 22:36:15 +0800

    fix: remove verify close button

--- app/dashboard/component/cert.tsx
+++ app/dashboard/component/cert.tsx
@@ -1,50 +1,50 @@
 'use client'
 
-import { Button, Input, Modal, ModalBody, ModalContent, ModalHeader, useDisclosure, useModal } from "@nextui-org/react"
-import { Component, Dispatch, SetStateAction, useEffect, useMemo, useState } from "react"
+import {Button, Input, Modal, ModalBody, ModalContent, ModalHeader, useDisclosure, useModal} from "@nextui-org/react"
+import {Component, Dispatch, SetStateAction, useEffect, useMemo, useState} from "react"
 import {QRCodeSVG} from 'qrcode.react';
 import useCert from "../hooks/use-cert";
 import {verify} from '../../store';
-import { useAtom } from "jotai";
-import { Message } from "@/components/message";
+import {useAtom} from "jotai";
+import {Message} from "@/components/message";
 
 interface CertInitProps {
-    prev: ()=>void
-    onClick: ()=>void;
+    prev: () => void
+    onClick: () => void;
     certName: string;
     certNo: string;
-    setCertName:  Dispatch<SetStateAction<string>>
-    setCertNo:  Dispatch<SetStateAction<string>>
+    setCertName: Dispatch<SetStateAction<string>>
+    setCertNo: Dispatch<SetStateAction<string>>
 }
 
 const CertInit = (props: CertInitProps) => {
-    const {certNo,certName,setCertNo,setCertName} = props;
+    const {certNo, certName, setCertNo, setCertName} = props;
     return (
         <>
-            <Input label="真实姓名" onValueChange={setCertName} value={certName} isRequired />
-            <Input label="身份证号" onValueChange={setCertNo} value={certNo} isRequired />
+            <Input label="真实姓名" onValueChange={setCertName} value={certName} isRequired/>
+            <Input label="身份证号" onValueChange={setCertNo} value={certNo} isRequired/>
             <Button className="w-fit" onClick={props.onClick}>下一步</Button>
         </>
     )
 }
 
-const CertVerify = (props: {cert_url: string, query: ()=>Promise<unknown>, prev: ()=>void}) => {
+const CertVerify = (props: { cert_url: string, query: () => Promise<unknown>, prev: () => void }) => {
     const {cert_url} = props;
     const [qrUrl, setQrUrl] = useState(cert_url);
     const [isVerify, setVerify] = useAtom(verify)
     const queryVerifyState = () => {
         props.query()
-        .then(()=>{
-            setVerify(true);
-        })
-        .catch(()=>{
-            setVerify(false);
-            Message.error('实名认证失败')
-        })
+            .then(() => {
+                setVerify(true);
+            })
+            .catch(() => {
+                setVerify(false);
+                Message.error('实名认证失败')
+            })
     }
     return (
         <>
-            <QRCodeSVG value={cert_url} size={320} bgColor="#000" fgColor="#fff" includeMargin={false} />
+            <QRCodeSVG value={cert_url} size={320} bgColor="#000" fgColor="#fff" includeMargin={false}/>
             <span>请使用支付宝扫描二维码</span>
             <div className="w-fit space-x-4">
                 <Button className="w-fit" onClick={props.prev}>上一步</Button>
@@ -54,21 +54,21 @@ const CertVerify = (props: {cert_url: string, query: ()=>Promise<unknown>, prev:
     )
 }
 
-export default function Cert(){
+export default function Cert() {
     const {isOpen, onOpenChange, onOpen} = useDisclosure();
     const {step, addStep, prev, certInitHook, certVerifyHook} = useCert();
     const CertSteps = (step: number) => {
         const steps = [
-            <CertInit key='init' {...certInitHook} prev={prev} />,
-            <CertVerify key='verify' cert_url={certVerifyHook.certUrl} query={certVerifyHook.onClick} prev={prev} />
+            <CertInit key='init' {...certInitHook} prev={prev}/>,
+            <CertVerify key='verify' cert_url={certVerifyHook.certUrl} query={certVerifyHook.onClick} prev={prev}/>
         ];
         return steps[step];
-    }    
-    useEffect(()=>{
+    }
+    useEffect(() => {
         onOpen();
-    },[])
+    }, [])
     return (
-        <Modal backdrop="blur" isOpen={isOpen}>
+        <Modal backdrop="blur" isOpen={isOpen} hideCloseButton>
             <ModalContent>
                 {(close) => (
                     <>
